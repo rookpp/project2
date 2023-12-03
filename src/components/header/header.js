d5a6e019dea38aab5b4351bf131f69d3985ca962
Kamil Holub <kahol20000@gmail.com>
2023-12-03 04:35:10 -0500

    login screen

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,22 +8,22 @@ import { MdNotifications, MdApps } from "react-icons/md"
 const Header = () => {
   return (
     <div className='border border-dark'>
-      <FaBars className="header__menu" size={26}/>
-      <img src="https://www.svgrepo.com/download/156646/play-button.svg" alt="logo" className="header__logo"/>
+      <FaBars className="header__menu" size={26} />
+      <img src="https://www.svgrepo.com/download/156646/play-button.svg" alt="logo" className="header__logo" />
       <form>
-        <input type="text" placeholder="Search"/>
+        <input type="text" placeholder="Search" />
         <button type="submit">
           <AiOutlineSearch size={22} />
         </button>
       </form>
       <div className="header__icons">
-        <MdNotifications size={28}/>
-        <MdApps size={28}/>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Icon-round-Question_mark.svg/640px-Icon-round-Question_mark.svg.png" alt="avatar"/>
+        <MdNotifications size={28} />
+        <MdApps size={28} />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Icon-round-Question_mark.svg/640px-Icon-round-Question_mark.svg.png" alt="avatar" />
       </div>
     </div>
   )
-  
+
 }
 
 export default Header
--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -2,29 +2,10 @@ import React from 'react'
 import "./_video.scss"
 const Video = () => {
   return (
-    <div className='video' onClick={handleVideoClick}>
-       <div className='video__top'>
-          {/* <img src={medium.url} alt='' /> */}
-          <LazyLoadImage src={medium.url} effect='blur' />
-          <span className='video__top__duration'>{_duration}</span>
-       </div>
-       <div className='video__title'>{title}</div>
-       <div className='video__details'>
-          <span>
-             <AiFillEye /> {numeral(views).format('0.a')} Views •{' '}
-          </span>{' '}
-          <span> {moment(publishedAt).fromNow()} </span>
-       </div>
-       {!channelScreen && (
-          <div className='video__channel'>
-             <LazyLoadImage src={channelIcon?.url} effect='blur' />
-
-             <p>{channelTitle}</p>
-          </div>
-       )}
+    <div>
+      video
     </div>
- )
-}
+  )
 }
 
 export default Video
