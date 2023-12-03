4aae5cc4b6b743028f428cd7dee0c3f67db71483
Kamil Holub <kahol20000@gmail.com>
2023-12-03 04:27:57 -0500

    Forgot to add all files

--- src/components/categoriesbar/CategoriesBar.js
+++ src/components/categoriesbar/CategoriesBar.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import './_categoriesBar.scss'
+
+
+const keywords = [
+  'All',
+  'React js',
+  'Angular js',
+  'React Native',
+  'use of API',
+  'Redux',
+  'Music',
+  'Algorithm Art ',
+  'Guitar',
+  'Bengali Songs',
+  'Coding',
+  'Cricket',
+  'Football',
+  'Real Madrid',
+  'Gatsby',
+  'Poor Coder',
+  'Shwetabh',
+]
+
 const CategoriesBar = () => {
+  const [activeElement, setActiveElement] = useState('All')
+  const handleClick = value => {
+    setActiveElement(value)
+    
+ }
   return (
-    <div>
-      bar
+    <div className='categoriesBar'>
+       {keywords.map((value, i) => (
+          <span
+          onClick={() => handleClick(value)}
+          key={i}
+          className={activeElement === value ? 'active' : ''}>
+          {value}
+       </span>
+       ))}
     </div>
-  )
+ )
 }
 
 export default CategoriesBar
--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import "./_video.scss"
-const video = () => {
+const Video = () => {
   return (
-    <div>
-      video
+    <div className='video' onClick={handleVideoClick}>
+       <div className='video__top'>
+          {/* <img src={medium.url} alt='' /> */}
+          <LazyLoadImage src={medium.url} effect='blur' />
+          <span className='video__top__duration'>{_duration}</span>
+       </div>
+       <div className='video__title'>{title}</div>
+       <div className='video__details'>
+          <span>
+             <AiFillEye /> {numeral(views).format('0.a')} Views •{' '}
+          </span>{' '}
+          <span> {moment(publishedAt).fromNow()} </span>
+       </div>
+       {!channelScreen && (
+          <div className='video__channel'>
+             <LazyLoadImage src={channelIcon?.url} effect='blur' />
+
+             <p>{channelTitle}</p>
+          </div>
+       )}
     </div>
-  )
+ )
+}
 }
 
-export default video
+export default Video
