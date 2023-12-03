a0130bd3075c137643990a203e6148ef5a59382f
Kamil Holub <kahol20000@gmail.com>
2023-12-03 04:46:02 -0500

    setup up firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDLZ41eVignzd3cxzlaXwhdqWWm9jdr_CY",
+    authDomain: "project-c972e.firebaseapp.com",
+    projectId: "project-c972e",
+    storageBucket: "project-c972e.appspot.com",
+    messagingSenderId: "403207452075",
+    appId: "1:403207452075:web:ec40e9b40c0522fbd94eee",
+    measurementId: "G-5EPQ3Y86J7"
+  };
+
+  firebase.initializeApp(firebaseConfig)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import App from './App';
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './App'
+import { Provider } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import "./_base.scss"
+import 'react-lazy-load-image-component/src/effects/blur.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+import './_base.scss'
+import store from './redux/store'
+
+ReactDOM.render(
+   <Provider store={store}>
+      <Router>
+         <App />
+      </Router>
+   </Provider>,
+
+   document.getElementById('root')
+)
