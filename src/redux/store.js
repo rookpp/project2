8a6ed89db0f738e25a0f61365fe196ab90b47c53
Kamil Holub <kahol20000@gmail.com>
2023-12-03 09:02:31 -0500

    expand homescreen code, store.js needs to be redone

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,37 @@
-import {createStore, applyMiddleware} from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 
-import {composeWithDevTools} from 'redux-devtools-extension'
-
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
-const initialState = {
-    name:"Kamil",
-    age:"18"
-}
 
-const reducer = (initialState)=>initialState
+import { authReducer } from './reducers/auth.reducer'
+import {
+   homeVideosReducer,
+   relatedVideoReducer,
+   searchedVideosReducer,
+   subscriptionsChannelReducer,
+   channelVideosReducer,
+} from './reducers/videos.reducer'
+import { selectedVideoReducer } from './reducers/videos.reducer'
+import { channelDetailsReducer } from './reducers/channel.reducer'
+import { commentListReducer } from './reducers/comments.reducer'
 
-const store = createStore(reducer,initialState,composeWithDevTools(applyMiddleware(thunk)))
+const rootReducer = combineReducers({
+   auth: authReducer,
+   homeVideos: homeVideosReducer,
+   selectedVideo: selectedVideoReducer,
+   channelDetails: channelDetailsReducer,
+   commentList: commentListReducer,
+   relatedVideos: relatedVideoReducer,
+   searchedVideos: searchedVideosReducer,
+   subscriptionsChannel: subscriptionsChannelReducer,
+
+   channelVideos: channelVideosReducer,
+})
+
+const store = createStore(
+   rootReducer,
+   {},
+   composeWithDevTools(applyMiddleware(thunk))
+)
 
 export default store
